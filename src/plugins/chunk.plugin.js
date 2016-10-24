0bc7908c99a642114472318857f6109d584f3742
kMeillet <robin.meillet@epitech.eu>
2016-10-25 00:24:38 +0200

    Fix chunk plugin default vendor options

--- src/plugins/chunk.plugin.js
+++ src/plugins/chunk.plugin.js
@@ -2,11 +2,9 @@
 
 const { optimize: { CommonsChunkPlugin } } = require('webpack');
 
-const wrapper = () => provider =>
-  new CommonsChunkPlugin({
-    name: 'vendor',
-    minChunks: Infinity,
-    filename: 'vendor.[chunkhash:8].js',
-  });
+const wrapper =
+  (chunky = { name: 'vendor', minChunks: Infinity, filename: 'vendor.[chunkhash:8].js', }) =>
+    provider =>
+      new CommonsChunkPlugin(chunky);
 
 module.exports = wrapper;
