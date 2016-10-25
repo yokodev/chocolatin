1bb3c72ca2e37148d6e1850d86db1e3386a105bc
kMeillet <robin.meillet@epitech.eu>
2016-10-25 03:56:07 +0200

    Browser js dev server

--- demo/browser-js/config.dev.js
+++ demo/browser-js/config.dev.js
@@ -2,9 +2,9 @@
 
 const {
   burn,
-  mixins: { Input, Output, Target, Watch },
-  loaders: { EsLint },
-  plugins: { Define, NoError },
+  mixins: { Input, Output, Server },
+  loaders: { Assets, Css, EsLint },
+  plugins: { Define, NoError, Browser, DevTool, HtmlGenerator, Hmr, Dashboard, InlineCss },
 } = require('chocolatin');
 
 const { DEV } = require('./metadata');
@@ -12,16 +12,29 @@ const { DEV } = require('./metadata');
 // Mixins, Loaders and Plugins
 module.exports = burn(
   [
-    Input({ index: ['./src/index.js'] }),
-    Output('./dist/'),
-    Target('node'),
-    Watch(),
+    Input({
+      vendor: ['webpack-dev-server/client?http://0.0.0.0:3003', 'webpack/hot/only-dev-server', './src/vendor.js'],
+      app: ['webpack-dev-server/client?http://0.0.0.0:3003', 'webpack/hot/only-dev-server', './src/index.js', './src/critical.css'],
+    }),
+    Output('/tmp/', '[name].js', 'http://localhost:3003/'),
+    Server('localhost', 3003),
   ],
   [
+    Assets(),
+    Css([
+      require('autoprefixer')({ browsers: ['last 2 versions', 'ie > 8'] }),
+      require('css-mqpacker')(),
+    ]),
     EsLint(),
   ],
   [
+    Browser('http://localhost', 3003),
     Define('development', DEV),
+    HtmlGenerator('./src/index.html'),
+    DevTool(true),
+    Hmr(),
+    Dashboard(),
+    InlineCss(),
     NoError(),
   ]
 );
