8c5036b149cf3fa5e5f39baeb430d91d2132ae6d
kMeillet <robin.meillet@epitech.eu>
2016-10-25 23:15:05 +0200

    ng2 rework vendor file with stack trace for zone

--- demo/angular2/src/vendor.ts
+++ demo/angular2/src/vendor.ts
@@ -1,3 +1,8 @@
+// Place extra-vendor her and Angular 2 polyfills
+// Like :
+//
+// import 'jquery';
+// import 'materialize/dist/materialize.min.js';
 import 'core-js';
 import 'zone.js/dist/zone';
 
@@ -5,5 +10,6 @@ declare const ENV: string;
 
 if (ENV === 'development') {
   Error.stackTraceLimit = Infinity;
+
   require('zone.js/dist/long-stack-trace-zone');
 }
